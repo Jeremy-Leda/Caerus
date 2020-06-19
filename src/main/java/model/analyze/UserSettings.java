99cdd8fd8eb6aa77b4452faf1f9607b47f0e253f
jerem <jerem@LAPTOP-E4JG0ENA>
2020-06-19 20:00:11 +0200

    Correction Bug detection warning

--- src/main/java/model/analyze/UserSettings.java
+++ src/main/java/model/analyze/UserSettings.java
@@ -593,9 +593,7 @@ public class UserSettings {
 				.sorted(Comparator.comparing(StructuredField::getOrder)).collect(Collectors.toList());
 		for (StructuredField structuredField : listStructuredField) {
 			List<String> listValues = mapFinal.get(structuredField.getFieldName());
-			if (null == listValues) {
-				listValues = new ArrayList<>();
-			}
+			listValues.removeIf(v -> StringUtils.isBlank(v));
 			mapFinalOrdered.put(structuredField.getFieldName(), listValues);
 		}
 		return mapFinalOrdered;
